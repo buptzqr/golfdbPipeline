276a696f5080f9772e9c9c1978ca416611e65749
zhangqingrui <zhangqingrui@bupt.edu.cn>
2021-03-22 23:40:54 +0800

    rgb  opt

---
+++ rgb_opt_stream/dataloader_2_stream.py
@@ -0,0 +1,147 @@
+import os.path as osp
+import os
+import cv2
+import pandas as pd
+import numpy as np
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+
+def ToTensor(sample):
+    """Convert ndarrays in sample to Tensors."""
+    images, labels = sample['images'], sample['labels']
+    # images= np.asarray(images)
+    nImages = []
+    for image in images:
+        if isinstance(image, np.ndarray):
+            pass
+        else:
+            image = image.numpy()
+        nImages.append(image)
+        # nImages.append(image)
+    images = np.asarray(nImages)
+    images = images.transpose((0, 3, 1, 2))
+    return {'images': torch.from_numpy(images).float().div(255.),
+            'labels': torch.from_numpy(labels).long()}
+
+
+def Normalize(sample, mean, std):
+    mean = torch.tensor(mean)
+    std = torch.tensor(std)
+    images, labels = sample['images'], sample['labels']
+    images.sub_(mean[None, :, None, None]).div_(std[None, :, None, None])
+    return {'images': images, 'labels': labels}
+    
+def Normalize_T(sample):
+    images, labels = sample['images'], sample['labels']
+    images = np.asarray(images)
+    images = images.astype(np.float32)
+    labels = np.asarray(labels)
+    # print(images.shape)
+    imgsMean = np.mean(images, axis=(1, 2))
+    imgsMean = imgsMean.reshape(-1, 1, 1, 3)
+    images = np.subtract(images, imgsMean)
+    images = images.transpose((0, 3, 1, 2))
+    images = torch.from_numpy(images)
+    labels = torch.from_numpy(labels)
+    return {'images': images, 'labels': labels}
+
+
+class GolfDB_T(Dataset):
+    def __init__(self, data_file, vid_dir, seq_length, transform=None, train=True):
+        self.df = pd.read_pickle(data_file)
+        self.vid_dir = vid_dir
+        self.seq_length = seq_length
+        self.transform = transform
+        self.train = train
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        a = self.df.loc[idx, :]  # annotation info
+        events = a['events']
+        # now frame #s correspond to frames in preprocessed video clips
+        events -= events[0]
+
+        images, labels = [], []
+        opticalFileFolder = osp.join(self.vid_dir, '{}'.format(a['id']))
+        # print(opticalFileFolder)
+        if self.train:
+            start_frame = np.random.randint(events[-1] + 1)
+            pos = start_frame
+            # 光流文件是从第一帧开始的
+            if pos == 0:
+                pos = 1
+            while len(images) < self.seq_length:
+                opticalFileName = osp.join(
+                    opticalFileFolder, '{:0>4d}.jpg'.format(pos))
+                if os.path.exists(opticalFileName):
+                    opticalArray = cv2.imread(opticalFileName)
+                    # opticalOri = np.fromfile(
+                    #     opticalFileName, np.float32, offset=12).reshape(160, 160, 2)
+                    # opticalArray = np.empty([160, 160, 3], np.float32)
+                    # opticalArray[..., 0] = 255
+                    # opticalArray[..., 1] = opticalOri[:, :, 0]
+                    # opticalArray[..., 2] = opticalOri[:, :, 1]
+
+                    if self.transform:
+                        opticalArray = self.transform(opticalArray)
+                    images.append(opticalArray)
+                    pos_adj = pos - 1
+
+                    if pos_adj in events[1:-1]:
+                        labels.append(np.where(events[1:-1] == pos_adj)[0][0])
+                    else:
+                        labels.append(8)
+                    pos += 1
+                else:
+                    pos = 1
+        else:
+            # full clip
+            # get files num
+            filesNum = -1
+            for ger in os.walk(opticalFileFolder):
+                filesNum = len(ger[2])
+            for pos in range(1, filesNum + 1):
+                opticalFileName = osp.join(
+                    opticalFileFolder, '{:0>4d}.jpg'.format(pos))
+                opticalArray = cv2.imread(opticalFileName)
+                # print(opticalFileName)
+                # opticalOri = np.fromfile(
+                #     opticalFileName, np.float32, offset=12).reshape(160, 160, 2)
+                # opticalArray = np.empty([160, 160, 3], np.float32)
+                # opticalArray[..., 0] = 255
+                # opticalArray[..., 1] = opticalOri[:, :, 0]
+                # opticalArray[..., 2] = opticalOri[:, :, 1]
+                # print(opticalFileName + "is ok")
+                if self.transform:
+                    opticalArray = self.transform(opticalArray)
+                images.append(opticalArray)
+                pos_adj = pos - 1
+                if pos_adj in events[1:-1]:
+                    labels.append(np.where(events[1:-1] == pos_adj)[0][0])
+                else:
+                    labels.append(8)
+        sample = {'images': images, 'labels': np.asarray(labels)}
+        sample = Normalize_T(sample)
+
+        return sample
+
+
+if __name__ == '__main__':
+
+    dataset = GolfDB_T(data_file='data/train_split_1.pkl',
+                       vid_dir='/home/zqr/data/optical/opticalFlowOri_160',
+                       seq_length=64,
+                       train=True)
+
+    data_loader = DataLoader(dataset, batch_size=1,
+                             shuffle=False, num_workers=6, drop_last=False)
+    count = 0
+    for i, sample in enumerate(data_loader):
+        images, labels = sample['images'], sample['labels']
+        # print(labels)
+        events = np.where(labels.squeeze() < 8)[0]
+        count += 1
+        print('{}-{} events: {}'.format(count, len(events), events))
---
+++ rgb_opt_stream/train.py
@@ -0,0 +1,93 @@
+import sys
+sys.path.append("..")
+from dataloader_2_stream import GolfDB_2_stream
+from model_2_stream import EventDetector
+from util import *
+import torch
+from torch.utils.data import DataLoader
+from torchvision import transforms
+import os
+from torch.utils.tensorboard import SummaryWriter
+from myeval import myeval
+from data.config import cfg
+import torch.nn as nn
+if __name__ == '__main__':
+    # training configuration
+    split = cfg.SPLIT
+    iterations = cfg.ITERATIONS
+    it_save = cfg.IT_SAVE  # save model every 100 iterations
+    n_cpu = cfg.CPU_NUM
+    seq_length = cfg.SEQUENCE_LENGTH
+    bs = cfg.BATCH_SIZE  # batch size
+    k = 10  # frozen layers
+
+    model = EventDetector(pretrain=True,
+                          width_mult=1.,
+                          lstm_layers=1,
+                          lstm_hidden=256,
+                          bidirectional=True,
+                          dropout=False)
+    # freeze_layers(k, model)
+    
+    model.train()
+
+    model = nn.DataParallel(model)
+    model.cuda()
+    dataset = GolfDB_2_stream(data_file='../data/train_split_{}.pkl'.format(split),
+                              vid_dir=cfg.OPT_RESIZE_FILE_PATH,
+                              seq_length=seq_length,
+                              train=True)
+
+    data_loader = DataLoader(dataset,
+                             batch_size=bs,
+                             shuffle=True,
+                             num_workers=n_cpu,
+                             drop_last=True)
+
+    # the 8 golf swing events are classes 0 through 7, no-event is class 8
+    # the ratio of events to no-events is approximately 1:35 so weight classes accordingly:
+    weights = torch.FloatTensor(
+        [1/8, 1/8, 1/8, 1/8, 1/8, 1/8, 1/8, 1/8, 1/35]).cuda()
+    criterion = torch.nn.CrossEntropyLoss(weight=weights)
+    optimizer = torch.optim.Adam(
+        filter(lambda p: p.requires_grad, model.parameters()), lr=0.001)
+
+    losses = AverageMeter()
+
+    # writer = SummaryWriter()
+
+    if not os.path.exists('models'):
+        os.mkdir('models')
+
+    i = 0
+    while i < iterations:
+        # for p in optimizer.param_groups:
+        #     print(p['lr'])
+        for sample in data_loader:
+            images, opt_images, labels = sample['images'].cuda(), sample['opt_images'].cuda(), sample['labels'].cuda()
+            logits = model(images,opt_images)
+            labels = labels.view(bs*seq_length)
+            loss = criterion(logits, labels)
+            optimizer.zero_grad()
+            loss.backward()
+            losses.update(loss.item(), images.size(0))
+            optimizer.step()
+            print('Iteration: {}\tLoss: {loss.val:.4f} ({loss.avg:.4f})'.format(
+                i, loss=losses))
+            i += 1
+            if i % it_save == 0:
+                for itm in model.state_dict():
+                    print(itm[0])
+                torch.save({'optimizer_state_dict': optimizer.state_dict(),
+                            'model_state_dict': model.state_dict()}, './models/swingnet_{}.pth.tar'.format(i))
+            if i == 1000:
+                for p in optimizer.param_groups:
+                    p['lr'] *= 0.1
+            if i == 2000:
+                for p in optimizer.param_groups:
+                    p['lr'] *= 0.1
+            if i == 10000:
+                for p in optimizer.param_groups:
+                    p['lr'] *= 0.1
+            if i == iterations:
+                break
